67d5c14298cab55279fed68cbb1f420684987518
Jonas W <hdgamer1404Jonas@gmail.com>
2025-05-02 21:57:55 +0200

    Websocket handling

--- src/index.ts
+++ src/index.ts
@@ -24,18 +24,38 @@ server.listen(3013, () => {
     });
 });
 
-
+// Express middleware to handle Sending encoded files to the client
+// Required because the files need to be packageed into the binary
 app.use((req:express.Request, res:express.Response, next:express.NextFunction) => {
     if (req.method !== 'GET') return next(); // This endpoint only needs to handle GET requests
-
     let cleanPath = req.path.replace(/^\/+/, '');
     if (cleanPath === '') cleanPath = 'index.html'; // Default to index.html if no path is provided
-
     const b64 = publicAssets[cleanPath];
     if (!b64) return next();
-
     const data = Buffer.from(b64, 'base64');
     const ext = path.extname(cleanPath).toLowerCase();
     const mime = ext.startsWith('.') ? ext.slice(1) : ext;
     res.type(mime).send(data);
 })
+
+// Websocket setup
+wss.on('connection', (ws) => {
+    console.log('Frontend has connected to the websocket!');
+    ws.on('close', () => {
+        console.log('Frontend has disconnected from the websocket!');
+    });
+});
+
+/*
+* @description Sends a message to the frontend via the websocket connection.
+* @param message The message to send to the frontend.
+* @returns void
+*/
+export async function sendMessageToFrontend(message: string) {
+    if (wss.clients.size === 0) return; // No clients connected, so we don't need to send a message
+    wss.clients.forEach((client) => {
+        if (client.readyState === ws.OPEN) {
+            client.send(message);
+        }
+    });
+}
