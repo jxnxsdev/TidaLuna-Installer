aad7a47c46966e5a4f36230ec471b1f71a7e7120
Jonas W <jonas@jxnxsdev.me>
2026-01-06 21:45:07 +0100

    Nuke sentry
    Get rid of sentry causing virus popups

--- src/utils/PopupHelper.ts
+++ src/utils/PopupHelper.ts
@@ -1,28 +1,25 @@
 import { sendMessageToFrontend } from "..";
 import { Popup, PopupButton } from "../types/Popup";
-import { getOrCreateSentryUser } from "../Logging/Sentry";
 import { WebsocketMessageTypes } from "../enums/WebsocketMessageTypes";
 
-
 export async function sendErrorHelpPopup() {
-    const sentryUserId = await getOrCreateSentryUser();
+  const popup: Popup = {
+    title: "Installation Error",
+    message:
+      "<h3>Unfortunately, an error occurred during the installation process.</h3>\n\nPlease try running the installer again.\nIn case the problem persists, please join the TidaLuna Discord server and open an issue thread there. Make sure to include the error log from the installer.\n\nIn your error report, tag <em>jxnxsdev</em>",
+    type: "error",
+    buttons: [
+      {
+        label: "Join Discord Server",
+        action: "open_url",
+        value: "https://discord.gg/jK3uHrJGx4",
+        color: "primary",
+      },
+    ],
+  };
 
-    const popup: Popup = {
-        title: "Installation Error",
-        message: "<h3>Unfortunately, an error occurred during the installation process.</h3>\n\nPlease try running the installer again.\nIn case the problem persists, please join the TidaLuna Discord server and open an issue thread there. Make sure to include the error log from the installer.\n\nIn your error report, tag <em>jxnxsdev</em> and send them the following error ID: <em>{sentry:" + sentryUserId + "}</em>",
-        type: "error",
-        buttons: [
-            {
-                label: "Join Discord Server",
-                action: "open_url",
-                value: "https://discord.gg/jK3uHrJGx4",
-                color: "primary"
-            }
-        ]
-    };
-
-    await sendMessageToFrontend({
-        type: WebsocketMessageTypes.POPUP_MESSAGE,
-        data: popup,
-    });
+  await sendMessageToFrontend({
+    type: WebsocketMessageTypes.POPUP_MESSAGE,
+    data: popup,
+  });
 }
